1d65e0876644f3837ca512fd273070ce739b5821
Yo <brianzaragoza25@gmail.com>
2022-11-17 10:54:18 +0100

    github commit

--- djangocms/settings.py
+++ djangocms/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7#jgn*rxa7vhm-_es6m6wc(gi1d+=j4u1qtdnnjv0va-)4*bj^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["awesomeapp.herokuapp.com"]
 
 
 # Application definition
